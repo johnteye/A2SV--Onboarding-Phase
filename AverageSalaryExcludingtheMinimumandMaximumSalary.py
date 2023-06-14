65aeab452facfdbf98f2de490918c06adab0cd38
John Teye Doku <86769906+johnteye@users.noreply.github.com>
2023-06-14 08:49:23 +0000

    Create AverageSalaryExcludingtheMinimumandMaximumSalary.py

---
+++ AverageSalaryExcludingtheMinimumandMaximumSalary.py
@@ -0,0 +1,8 @@
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        
+        salary.remove(max(salary))
+        salary.remove(min(salary))
+
+        average = sum(salary) / len(salary)
+        return average
