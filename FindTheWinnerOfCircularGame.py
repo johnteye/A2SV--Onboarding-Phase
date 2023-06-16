2a671d4fc28bc3d5954d1a65d0593b7a9747de9e
John Teye Doku <86769906+johnteye@users.noreply.github.com>
2023-06-16 16:22:58 +0000

    Update FindTheWinnerOfCircularGame.py

--- FindTheWinnerOfCircularGame.py
+++ FindTheWinnerOfCircularGame.py
@@ -1,11 +1,23 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        res = 0
-        for i in range(1, n + 1):
-            res = (res + k) % i
-        return res + 1
+    
+        new_k = k -1
+        array = [i for i in range(1, n + 1)] #[1 2 3 4 5]
+
+        index = 0
+        while len(array) > 1:
+            #for index in range(0, 2 * len(array)):
+            #
+            value = array[(index + new_k) % len(array)]
+            rem_pos = (index + new_k) % len(array)
+            array.remove(value)
+            #[1 3 4 5]
+            index = rem_pos
+        for val in array:
+            return val
+    
       
       """
-      Time Complexity:O(N)
+      Time Complexity:O(N2)
       Space Complexity: O(1)
       """
