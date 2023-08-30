edfcddbd36df431f9dd7ec1f26f32710ddc86e98
John Teye Doku <86769906+johnteye@users.noreply.github.com>
2023-08-30 10:29:22 +0000

    Create RunningSum1D.py

---
+++ RunningSum1D.py
@@ -0,0 +1,14 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        running_sum = [0]* len(nums)
+        running_sum[0] = nums[0]
+        for i in range(1,len(nums)):
+            running_sum[i] = running_sum[i-1] + nums[i]
+            
+        return running_sum
+
+
+"""
+time complexity: O(n)
+space complexity: O(1)
+"""
