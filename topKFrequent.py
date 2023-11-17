a123d8fc0cb8a9f9ec1ca013ff5a583e142401b6
John Teye Doku <86769906+johnteye@users.noreply.github.com>
2023-11-17 09:21:29 +0000

    Update topKFrequent.py

--- topKFrequent.py
+++ topKFrequent.py
@@ -1,14 +1,28 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        
         count = Counter(words)
-        count = OrderedDict(count)
-        sorted_count = OrderedDict(sorted(count.items(), key=lambda x: (-x[1], x[0])))
-        res = []
-        # print(sorted_count)
-        for val in sorted_count:
-            res.append(val)
-            if len(res) == k:
-                return res
-
+        heap  = []
+        # print(count)
+        for key, val in count.items():
+            heappush(heap, (-val, key))
+        # print(heap)
+        res = [heappop(heap)[1] for _ in range(k)]          
+        # print(heap)
         return res
+        
+
+
+# class Solution:
+#     def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        
+#         count = Counter(words)
+#         count = OrderedDict(count)
+#         sorted_count = OrderedDict(sorted(count.items(), key=lambda x: (-x[1], x[0])))
+#         res = []
+#         # print(sorted_count)
+#         for val in sorted_count:
+#             res.append(val)
+#             if len(res) == k:
+#                 return res
+
+#         return res
