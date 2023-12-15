5cdd973a9cddde0c1230101b63b9638c187f8eef
John Teye Doku <86769906+johnteye@users.noreply.github.com>
2023-12-15 03:54:04 +0000

    Create openLock.py

---
+++ openLock.py
@@ -0,0 +1,35 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+
+        def moves(pos):
+            res = []
+            for i in range(len(pos)):
+                pos_new = pos
+                pos_new = pos[:i] +  str((int(pos_new[i]) + 1) % 10) + pos[i+1:]
+                res.append(pos_new)
+
+            for i in range(len(pos)):
+                pos_new = pos
+                pos_new = pos[:i] +  str((int(pos_new[i]) - 1 ) % 10) + pos[i+1:]
+                res.append(pos_new)
+            return res
+
+    
+        start = ("0000", 0)
+        queue = deque([start])
+        visited = set()
+
+        if start[0] in deadends:
+            return -1
+            
+        while queue:
+            curr, step = queue.popleft()
+            if curr == target:
+                return step
+
+            for move in moves(curr):
+                if move not in deadends and move not in visited:
+                    visited.add(move)
+                    queue.append((move, step+1))
+
+        return -1
